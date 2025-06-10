a8242b75e38440be0087c84b0ef9817d42f5f89f
defaultgnome <defaultgnome@users.noreply.github.com>
2025-06-10 23:33:26 +0300

    create other tables schemas

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,8 +1,8 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
-import { sqliteTableCreator } from "drizzle-orm/sqlite-core";
+import { relations, sql } from "drizzle-orm";
+import { primaryKey, real, sqliteTableCreator } from "drizzle-orm/sqlite-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -21,3 +21,63 @@ export const rawNotes = createTable("raw_note", (d) => ({
     .notNull(),
   updatedAt: d.integer({ mode: "timestamp" }).$onUpdate(() => new Date()),
 }));
+
+export const notes = createTable("note", (d) => ({
+  id: d.integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  sector: d.text(),
+  location: d.text(),
+  lat: real("lat"),
+  lng: real("lng"),
+  date: d.text(),
+  time: d.text(),
+  description: d.text().notNull(),
+  eventType: d.text(),
+  createdAt: d
+    .integer({ mode: "timestamp" })
+    .default(sql`(unixepoch())`)
+    .notNull(),
+  updatedAt: d.integer({ mode: "timestamp" }).$onUpdate(() => new Date()),
+}));
+
+export const rawNotesToNotes = createTable(
+  "raw_notes_to_notes",
+  (d) => ({
+    rawNoteId: d
+      .integer("raw_note_id", { mode: "number" })
+      .notNull()
+      .references(() => rawNotes.id, { onDelete: "cascade" }),
+    noteId: d
+      .integer("note_id", { mode: "number" })
+      .notNull()
+      .references(() => notes.id, { onDelete: "cascade" }),
+    isConfirmed: d
+      .integer("is_confirmed", { mode: "boolean" })
+      .default(false)
+      .notNull(),
+  }),
+  (t) => ({
+    pk: primaryKey({ columns: [t.rawNoteId, t.noteId] }),
+  }),
+);
+
+export const rawNotesRelations = relations(rawNotes, ({ many }) => ({
+  notes: many(rawNotesToNotes),
+}));
+
+export const notesRelations = relations(notes, ({ many }) => ({
+  rawNotes: many(rawNotesToNotes),
+}));
+
+export const rawNotesToNotesRelations = relations(
+  rawNotesToNotes,
+  ({ one }) => ({
+    rawNote: one(rawNotes, {
+      fields: [rawNotesToNotes.rawNoteId],
+      references: [rawNotes.id],
+    }),
+    note: one(notes, {
+      fields: [rawNotesToNotes.noteId],
+      references: [notes.id],
+    }),
+  }),
+);
